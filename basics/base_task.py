5bfd711940fe9630a60fc0d39ccaa881b756d384
Erythrocyte3803 <2544390577@qq.com>
2025-06-09 10:41:29 +0900

    fix

--- basics/base_task.py
+++ basics/base_task.py
@@ -399,10 +399,10 @@ class BaseTask(pl.LightningModule):
         if "ep" in hparams['val_check_interval']:
             check_interval = hparams['val_check_interval'].replace('ep', '')
             val_check_interval_ep = int(check_interval) * hparams['accumulate_grad_batches']
-            val_check_interval_steps = 0
+            val_check_interval_steps = None
         else:
             val_check_interval_steps = hparams['val_check_interval'] * hparams['accumulate_grad_batches']
-            val_check_interval_ep = 0
+            val_check_interval_ep = None
             
         if "ep" in hparams['max_updates']:
             max_updates_ep = int(hparams['max_updates'].replace('ep', ''))
